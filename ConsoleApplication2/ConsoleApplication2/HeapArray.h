72ac0e0cac9404a8eb6dabee8fe14c7ac0853c88
HangHangbo <1679759074@qq.com>
2019-02-26 22:38:21 +0800

    链表-2

---
+++ ConsoleApplication2/ConsoleApplication2/HeapArray.h
@@ -0,0 +1,25 @@
+#pragam once
+
+  //向下调整
+//array数组的开始位置
+//size 数组的长度
+//root  要调整的结点位置
+void AdjustDown(int array[])
+
+
+
+
+void ADjustUp(int array[], int size, int child)
+{
+	if (child == 0)
+	{
+	}
+	int parent = (size - 1) / 2;
+	while (1)
+	{
+		if (array[child] >array[parent])
+		{
+			
+		}
+	}
+}
